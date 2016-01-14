3a84492ed937805448f1b59fc752d50f81c338c0
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2016-01-14 09:00:02 +0100

    Update Random plugin to return a random address.

--- src/Plugin/GeocoderProvider/Random.php
+++ src/Plugin/GeocoderProvider/Random.php
@@ -37,15 +37,84 @@ class Random extends GeocoderProvider {
   }
 
   public function geocode($data) {
-    $result = array(
-      'latitude' => mt_rand(0, 90),
-      'longitude' => mt_rand(-180, 180),
-    );
+    $cid = $this->getCacheCid($data);
 
-    return $this->factory->createFromArray(array($result));
+    if ($value = $this->cache_get($cid)) {
+      return $value;
+    }
+
+    try {
+      $value = $this->factory->createFromArray(array($this->getRandomResult()));
+      $this->cache_set($cid, $value);
+    } catch (\Exception $e) {
+      $this->loggerChannel->error($e->getMessage(), array('channel' => 'geocoder'));
+      $this->messenger->addMessage($e->getMessage(), 'error', FALSE);
+      $value = FALSE;
+    }
+
+    return $value;
   }
 
   public function reverse($latitude, $longitude) {
-    // TODO.
+    $cid = $this->getCacheCid($latitude, $longitude);
+
+    if ($value = $this->cache_get($cid)) {
+      return $value;
+    }
+
+    try {
+      $result = $this->getRandomResult();
+      $result['latitude'] = $latitude;
+      $result['longitude'] = $longitude;
+
+      $value = $this->factory->createFromArray(array($result));
+      $this->cache_set($cid, $value);
+    } catch (\Exception $e) {
+      $this->loggerChannel->error($e->getMessage(), array('channel' => 'geocoder'));
+      $this->messenger->addMessage($e->getMessage(), 'error', FALSE);
+      $value = FALSE;
+    }
+
+    return $value;
   }
+
+  private function getRandomCountryInfo($type = NULL) {
+    include_once DRUPAL_ROOT . '/includes/locale.inc';
+    $countries = country_get_list();
+    uksort($countries, function() { return rand() > rand(); });
+    $country = array_slice($countries, 0, 1);
+
+    $value = array(
+      'code' => key($country),
+      'name' => reset($country),
+    );
+
+    if (is_null($type)) {
+      return $value;
+    }
+
+    return isset($value[$type]) ? $value[$type] : $value;
+  }
+
+  /**
+   * Generate a fake random address array.
+   *
+   * @return array
+   */
+  private function getRandomResult() {
+    $country = $this->getRandomCountryInfo();
+    $streetTypes = array('street', 'avenue', 'square', 'road', 'way', 'drive', 'lane', 'place', 'hill', 'gardens', 'park');
+
+    return array(
+      'latitude' => mt_rand(0, 90) + mt_rand() / mt_getrandmax(),
+      'longitude' => mt_rand(-180, 180) + mt_rand() / mt_getrandmax(),
+      'streetName' => $this->getRandomCountryInfo('name') . ' ' . $streetTypes[mt_rand(0, count($streetTypes) -1)],
+      'streetNumber' => mt_rand(1, 1000),
+      'postalCode' => mt_rand(1, 1000),
+      'locality' => sha1(mt_rand()/mt_getrandmax()),
+      'country' =>  $country['name'],
+      'countryCode' => $country['code'],
+    );
+  }
+
 }
