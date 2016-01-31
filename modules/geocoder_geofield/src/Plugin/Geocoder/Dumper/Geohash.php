9e44a4c83a27621ece867d4f0a13772bcc84f647
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2016-01-31 14:36:47 +0100

    Fix Geohash and Geometry dumper.

--- modules/geocoder_geofield/src/Plugin/Geocoder/Dumper/Geohash.php
+++ modules/geocoder_geofield/src/Plugin/Geocoder/Dumper/Geohash.php
@@ -6,10 +6,10 @@
 
 namespace Drupal\geocoder_geofield\Plugin\Geocoder\Dumper;
 
+use Drupal\geocoder\Plugin\Geocoder\Dumper\GeoJson;
 use Drupal\geocoder\Plugin\Geocoder\DumperInterface;
 use Drupal\geocoder\Plugin\Geocoder\DumperBase;
 use Drupal\geophp\GeoPHPInterface;
-use Geocoder\Model\Address;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -20,39 +20,17 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *  name = "Geohash"
  * )
  */
-class Geohash extends Dumper\GeoJson implements DumperInterface {
+class Geohash extends GeoJson implements DumperInterface {
   /**
-   * @var GeoPHPInterface
-   */
-  private $geophp;
-
-  /**
-   * @inheritDoc
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, \Geocoder\Dumper\Dumper $dumper, GeoPHPInterface $geophp) {
-    $this->geophp = $geophp;
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $dumper);
-  }
-
-  /**
-   * @inheritDoc
+   * @inheritdoc
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return array(
-      $container,
+    return new static(
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('geocoder.dumper.geojson'),
-      $container->get('geophp.geophp'),
+      $container->get('geocoder.dumper.geohash')
     );
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function dump(Address $address) {
-    return $this->geophp->load(parent::dump($address), 'json')->out('geohash');
   }
 
 }
